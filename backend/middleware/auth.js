50c48f55a0d7da782b68053a3757f47c84a5262b
Abhiram0703 <abhiram072005@gmail.com>
2025-06-07 21:08:04 +0530

    added files

---
+++ backend/middleware/auth.js
@@ -0,0 +1,25 @@
+const jwt = require('jsonwebtoken')
+
+const midwareFunc = (req, res, next) => {
+  let jwtToken
+  const auth = req.headers['authorization']
+  if (auth !== undefined) {
+    jwtToken = auth.split(' ')[1]
+  }
+  if (jwtToken === undefined) {
+    res.status(401)
+    res.send('Invalid JWT Token')
+  } else {
+    jwt.verify(jwtToken, 'MY_SECRET_TOKEN', async (error, payload) => {
+      if (error) {
+        res.status(401)
+        res.send('Invalid JWT Token')
+      } else {
+        req.user = payload
+        next()
+      }
+    })
+  }
+}
+
+module.exports = midwareFunc;
